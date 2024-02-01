4e411158be3e00ec6877407bc6d3589f2de048d1
Huy0110 <duchuy01102001@gmail.com>
2024-02-01 09:16:32 +0700

    update: authen

--- src/services/farm.service.js
+++ src/services/farm.service.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const farmModel = require('../models/farm.model')
+const { Types } = require('mongoose')
 
 const findByEmail = async ({
   email,
@@ -15,6 +16,22 @@ const findByEmail = async ({
   return await farmModel.findOne({ email }).select(select).lean().exec()
 }
 
+const getFarm = async ({ farmId }) => {
+  const foundFarm = await farmModel
+    .findOne({
+      _id: new Types.ObjectId(farmId)
+    })
+    .exec()
+
+  return {
+    _id: foundFarm._id,
+    name: foundFarm.name,
+    email: foundFarm.email,
+    roles: foundFarm.roles
+  }
+}
+
 module.exports = {
-  findByEmail
+  findByEmail,
+  getFarm
 }
