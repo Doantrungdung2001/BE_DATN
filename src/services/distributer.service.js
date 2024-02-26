67d6c76bb3b5c5e1eea57c0b011b177625b9d796
Huy0110 <duchuy01102001@gmail.com>
2024-02-26 18:01:38 +0700

    Add: add and update distributer api

--- src/services/distributer.service.js
+++ src/services/distributer.service.js
@@ -1,5 +1,10 @@
 const { Types } = require('mongoose')
-const { getAllDistributers, getDistributerById } = require('../models/repositories/distributer.repo')
+const {
+  getAllDistributers,
+  getDistributerById,
+  addDistributer,
+  updateDistributer
+} = require('../models/repositories/distributer.repo')
 const { MethodFailureError, BadRequestError, NotFoundError } = require('../core/error.response')
 const { isValidObjectId } = require('../utils')
 
@@ -22,6 +27,26 @@ class DistributerService {
     }
 
     return foundDistributer
+  }
+
+  static async addDistributer({ distributerData }) {
+    if (!distributerData) throw new BadRequestError('Distributer data is required')
+    const newDistributer = await addDistributer({ distributerData })
+    return newDistributer
+  }
+
+  static async updateDistributer({ distributerId, distributerData }) {
+    if (!distributerId) throw new BadRequestError('Distributer id is required')
+    if (!isValidObjectId(distributerId)) {
+      throw new BadRequestError('Invalid distributer id')
+    }
+
+    const updatedDistributer = await updateDistributer({ distributerId, distributerData })
+    if (!updatedDistributer) {
+      throw new MethodFailureError('Failed to update distributer')
+    }
+
+    return updatedDistributer
   }
 }
 
